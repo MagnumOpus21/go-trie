b4fb6a8b06178c84df59181bf8ead66364b2160c
MagnumOpus21 <sivaauturic@gmail.com>
2018-08-05 18:40:35 -0400

    Added PrefixSearch. Fixed locks during add

--- trienode/trie.go
+++ trienode/trie.go
@@ -2,6 +2,7 @@ package trienode
 
 import (
 	"fmt"
+	"sort"
 )
 
 type trieFunctions interface {
@@ -16,6 +17,25 @@ type Trie struct {
 	Words uint64
 }
 
+//Words is a string array
+type Words []string
+
+// Sort interface
+func (w Words) Len() int {
+	return len(w)
+}
+
+func (w Words) Swap(a, b int) {
+	w[a], w[b] = w[b], w[a]
+}
+
+func (w Words) Less(a, b int) bool {
+	if len(w[a]) >= len(w[b]) {
+		return w[a] <= w[b]
+	}
+	return w[a] > w[b]
+}
+
 // New returns a new Trie
 func New() *Trie {
 	rootInit := new(Node)
@@ -25,20 +45,51 @@ func New() *Trie {
 
 // addChildNode returns a new child node if the current node is nil
 func (n *Node) addChildNode(letter rune, metaData interface{}, leafCheck bool) *Node {
+	if leafCheck == true {
+		metaData = nil
+	}
 	return &Node{letter: letter, level: n.level + 1, metadata: metaData,
 		leaf: !leafCheck, children: make(map[rune]*Node)}
+}
+
+func (n *Node) prefixSearch(prefix []rune) (matches Words) {
+	node := n
+	prefixFound := make([]rune, 0)
+	for _, letter := range prefix {
+		if nodeCheck, ok := node.children[letter]; ok {
+			node = nodeCheck
+			prefixFound = append(prefixFound, letter)
+		} else {
+			break
+		}
+	}
+	matches = node.prefixSearchHelper(prefix, matches)
+	sort.Sort(matches)
+	return matches
+}
+
+func (n *Node) prefixSearchHelper(prefix []rune, match []string) []string {
+	if n.leaf {
+		match = append(match, string(prefix))
+	}
+	for _, candidate := range n.children {
+		newPrefix := append(prefix, candidate.letter)
+		tempMatch := candidate.prefixSearchHelper(newPrefix, make([]string, 0))
+		for _, word := range tempMatch {
+			match = append(match, word)
+		}
+	}
+	return match
 }
 
 // remove tries to delete a word from the Trie
 func (n *Node) remove(word []rune) {
 	if len(word) == 1 {
-		fmt.Println("Hurr ", word, n)
 		if len(n.children[word[0]].children) == 0 {
 			delete(n.children, word[0])
 		} else {
 			n.children[word[0]].leaf = false
 		}
-		fmt.Println("Remove: ", n.children)
 		return
 	}
 	n.children[word[0]].remove(word[1:])
@@ -50,7 +101,6 @@ func (t *Trie) Find(word string) (status bool) {
 	wordRuned := []rune(word)
 	node := t.Root
 	for _, letter := range wordRuned {
-		fmt.Println(letter, node)
 		if nodeChild, ok := node.children[letter]; ok {
 			node = nodeChild
 			status = node.leaf
@@ -75,8 +125,10 @@ func (t *Trie) Add(word string, metaData interface{}) (found bool, err error) {
 		if nodeChild, ok := node.children[letter]; ok {
 			node = nodeChild
 			if index == len(wordRuned)-1 {
+				node.locks.Lock()
 				node.leaf = true
 				node.metadata = metaData
+				node.locks.Unlock()
 			}
 		} else {
 			//Obtain a lock on this node to prevent other go-routines
@@ -101,4 +153,10 @@ func (t *Trie) Remove(word string) (status bool, err error) {
 	node.remove(wordRune)
 	t.Words--
 	return true, nil
+}
+
+// PrefixSearch returns all the words that have the given prefix
+func (t *Trie) PrefixSearch(prefix string) (matches []string) {
+	matches = t.Root.prefixSearch([]rune(prefix))
+	return matches
 }
